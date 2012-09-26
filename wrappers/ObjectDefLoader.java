6c4ef9a5d1b56c4497c95705226e12942c66140a
Marneus901 <circadian@live.com>
2012-09-25 23:52:07 -0400

    v0.6; new cache added

--- wrappers/ObjectDefLoader.java
+++ wrappers/ObjectDefLoader.java
@@ -9,6 +9,7 @@
 ********************************************************/
 package org.dynamac.bot.api.wrappers;
 
+import org.dynamac.bot.api.methods.Nodes;
 import org.dynamac.enviroment.Data;
 import org.dynamac.enviroment.hook.ClassHook;
 import org.dynamac.enviroment.hook.FieldHook;
@@ -21,8 +22,17 @@ public class ObjectDefLoader {
 		currentObject = o;
 		currentHook = Data.indentifiedClasses.get("ObjectDefLoader");
 	}
-	public Cache getCache(){
-		FieldHook fh = currentHook.getFieldHook("getCache");
+	public Cache getDefCache(){
+		FieldHook fh = currentHook.getFieldHook("getDefCache");
+		if(fh!=null){
+			Object data = fh.getData(currentObject);
+			if(data!=null)
+				return new Cache(data);
+		}
+		return null;
+	}
+	public Cache getModelCache(){
+		FieldHook fh = currentHook.getFieldHook("getModelCache");
 		if(fh!=null){
 			Object data = fh.getData(currentObject);
 			if(data!=null)
@@ -47,5 +57,16 @@ public class ObjectDefLoader {
 				return (String[])data;
 		}
 		return new String[]{};
+	}
+	public ObjectDef getObjectDef(int id){
+		Cache cache = getDefCache();
+		if(cache!=null){
+			Node node = Nodes.lookup(cache.getTable(), id);
+			if(node!=null){
+				ObjectDef def = new ObjectDef(node.currentObject);
+				return def;
+			}
+		}
+		return null;
 	}
 }
